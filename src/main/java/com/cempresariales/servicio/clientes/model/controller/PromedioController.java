32c63694263c1eb2780139804a253b6f7aab2fc8
WilsonCastro1988 <wilson.castro@epn.edu.ec>
2020-06-08 13:15:40 -0500

    charge initial

--- src/main/java/com/cempresariales/servicio/clientes/model/controller/PromedioController.java
+++ src/main/java/com/cempresariales/servicio/clientes/model/controller/PromedioController.java
@@ -3,18 +3,21 @@ package com.cempresariales.servicio.clientes.model.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cempresariales.servicio.commons.model.entity.Promedio;
 import com.cempresariales.servicio.clientes.model.service.PromedioServiceImp;
 
 @RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS })
 @RequestMapping(value = "promedio")
 public class PromedioController {
 
