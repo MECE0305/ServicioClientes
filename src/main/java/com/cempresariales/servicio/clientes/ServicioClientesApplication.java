ec4eb0e7d6aed38d6a73c786ba5009d601d2d69f
WilsonCastro1988 <yagamichester@gmail.com>
2021-05-15 21:15:45 -0500

    Restructuracion IGVM virus

--- src/main/java/com/cempresariales/servicio/clientes/ServicioClientesApplication.java
+++ src/main/java/com/cempresariales/servicio/clientes/ServicioClientesApplication.java
@@ -6,8 +6,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan({"com.cempresariales.servicio.commons.model.entity"})
+//public class ServicioClientesApplication extends SpringBootServletInitializer{
 public class ServicioClientesApplication {
 
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServicioClientesApplication.class);
+	}*/
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioClientesApplication.class, args);
 	}
